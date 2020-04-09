4164152075e6e9731c7c1a6349f42888480ecdb8
Jorge Ramirez <joramirez96@gmail.com>
2020-04-09 19:25:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,11 @@
 class Triangle
   # write code here
+  def initialize(:equilateral, :isosceles, :scalene)
+    @equilateral = equilateral
+    @isosceles = isosceles
+    @scalene = scalene
+  end
+
+  class TriangleError < StandardError
+  end 
 end
